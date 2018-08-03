5129045e37f474636643dc7b029c7aec8f6bd488
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-08-03 15:45:35 +0530

    " -> 3 of 4 passed. pylint: 9.00/10 "

--- M5/p1/perfect_cube.py
+++ M5/p1/perfect_cube.py
@@ -11,11 +11,11 @@
 def main():
     """code is here"""
     guess = 0
-    x_val = int(input("Enter an integer: "))
+    x_val = int(input())
     while guess**3 < x_val:
         guess = guess + 1
     if guess**3 == x_val:
-        print("Cube root of", x_val, "is", guess)
+        print(x_val,"is a perfect cube")
     else:
         print(x_val, "is not a perfect cube")
 if __name__ == "__main__":
