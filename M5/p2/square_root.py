985903835a872d549b48115fde1dcc5d950e0eb0
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-08-03 18:51:51 +0530

    " -> 0 of 3 passed. pylint: 9.33/10 "

--- M5/p2/square_root.py
+++ M5/p2/square_root.py
@@ -1,7 +1,14 @@
-"""Write a python program to find the square root of the given number using approximation method"""
+"""Write a python program to find the square root of the given number
+using approximation method
+testcase 1
+input: 25
+output: 4.999999999999998
+
+testcase 2
+input: 49
+output: 6.999999999999991"""
 def main():
-    """your code here
-    """
+    """your code here"""
     square = int(input())
     epsilon = 0.01
     guess = 0.0
@@ -17,4 +24,4 @@ def main():
         print(guess, 'is close to the square root of', square)
 
 if __name__ == "__main__":
-    main()
+   main()
