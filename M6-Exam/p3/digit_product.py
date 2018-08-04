9a989f1ff383e01d2d4bea7db81946ec9efd65a1
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-08-04 11:19:09 +0530

    " -> 4 of 7 passed. pylint: 10.00/10 "

--- M6-Exam/p3/digit_product.py
+++ M6-Exam/p3/digit_product.py
@@ -11,10 +11,14 @@ def main():
     g_num = int(input())
     rem_num = 0
     pro_num = 1
-    while g_num != 0:
-        rem_num = g_num % 10
-        pro_num = pro_num * rem_num
-        g_num = round(g_num / 10)
-    print(pro_num)
+    if g_num == 0:
+        print(0)
+    else:
+        while g_num != 0:
+            rem_num = g_num % 10
+            pro_num = pro_num * rem_num
+            g_num = round(g_num / 10)
+        print(pro_num)
+
 if __name__ == "__main__":
     main()
