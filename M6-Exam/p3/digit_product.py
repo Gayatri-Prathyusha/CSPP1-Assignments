8d4b9236227b87ece8414c1331a9aa84852b6dfb
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-08-04 11:50:23 +0530

    " -> 6 of 7 passed. pylint: 8.50/10 "

--- M6-Exam/p3/digit_product.py
+++ M6-Exam/p3/digit_product.py
@@ -8,11 +8,19 @@ def main():
     """
     Read any number from the input, store it in variable int_input.
     """
-    g_num = abs(int(input()))
+    g_num = int(input())
     rem_num = 0
     pro_num = 1
     if g_num == 0:
         print(0)
+    elif g_num<0:
+        g_num= abs(g_num)
+        while g_num != 0:
+            rem_num = g_num % 10
+            pro_num = pro_num * rem_num
+            g_num = g_num // 10
+
+        print("-",pro_num)
     else:
         while g_num != 0:
             rem_num = g_num % 10
