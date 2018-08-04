868283d9dd206af90a5cbaeac855847b6233032f
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-08-04 10:57:48 +0530

    " -> 2 of 7 passed. pylint: 10.00/10 "

--- M6-Exam/p3/digit_product.py
+++ M6-Exam/p3/digit_product.py
@@ -1,15 +1,20 @@
-'''
-Given a  number int_input, find the product of all the digits
-example: 
-	input: 123
-	output: 6
-'''
+"""
+Given a number int_input, find the product of all the digits
+example:
+    input: 123
+    output: 6
+"""
 def main():
-    '''
+    """
     Read any number from the input, store it in variable int_input.
-    '''
-    int_input = int(input())
-
-
+    """
+    g_num = int(input())
+    rem_num = 0
+    pro_num = 1
+    while g_num != 0:
+        rem_num = g_num % 10
+        pro_num = pro_num * rem_num
+        g_num = round(g_num / 10)
+    print(pro_num)
 if __name__ == "__main__":
     main()
