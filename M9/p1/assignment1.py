d3c5f78945a3b54e7e8a94f6eb9ee58ce973def2
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-08-08 18:11:21 +0530

    " -> 0 of 9 passed. pylint: 0/0 "

--- M9/p1/assignment1.py
+++ M9/p1/assignment1.py
@@ -15,19 +15,20 @@ def is_word_guessed(secret_word, letters_guessed):
       False otherwise
     '''
     # FILL IN YOUR CODE HERE...
-    for i in secretWord:
-        if i not in lettersGuessed:
-            return False
-    return True
-    pass
-
+"""guess"""
+    list_1 = list(secret_word)
+    length_1 = len(secret_word)
+    count = 0
+    for i in list_1:
+        for j in letters_guessed:
+            if j == i:
+                count = count+1
+    return bool(count == length_1)
 def main():
-    '''
-    Main function for the program
-    '''
+    """guess"""
     user_input = input()
     if user_input:
-        data = user_input.split()
+        data = user_input.split(" ")
         secret_word = data[0]
     else:
         data = []
@@ -36,6 +37,5 @@ def main():
     for j in range(1, len(data)):
         list1.append(data[j][0])
     print(is_word_guessed(secret_word, list1))
-
 if __name__ == "__main__":
     main()
