c97333e7402caa4752495286deb12052e7e7ed54
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-08-11 00:09:00 +0530

    " -> 5 of 5 passed. pylint: -1.33/10 "

--- M11/p4/assignment4.py
+++ M11/p4/assignment4.py
@@ -1,25 +1,33 @@
-#Exercise: Assignment-4
-#We are now ready to begin writing the code that interacts with the player. We'll be implementing the playHand function. This function allows the user to play out a single hand. First, though, you'll need to implement the helper calculateHandlen function, which can be done in under five lines of code.
+"""#Exercise: Assignment-4
+#We are now ready to begin writing the code
+that interacts with the player. We'll be implementing
+the playHand function. This function allows the user
+to play out a single hand. First, though, you'll need
+to implement the helper calculate_handlen function, which
+can be done in under five lines of code."""
 
 
-def calculateHandlen(hand):
+def calculate_handlen(hand):
     """ 
     Returns the length (number of letters) in the current hand.
     
     hand: dictionary (string int)
     returns: integer
     """
-    # TO DO... <-- Remove this comment when you code this function
-    
+    sum_o = 0
+    for letters in hand:
+    	sum_o= sum_o + hand[letters]
+    return sum_o
 
 def main():
-	n=input()
-	adict={}
-	for i in range(int(n)):
-		data=input()
-		l=data.split()
-		adict[l[0]]=int(l[1])
-	print(calculateHandlen(adict))
+	"""main function"""
+	n = input()
+	a_dict = {}
+	for _ in range(int(n)):
+		data = input()
+		l_ist = data.split()
+		a_dict[l_ist[0]] = int(l_ist[1])
+	print(calculate_handlen(a_dict))
 		
 
 
