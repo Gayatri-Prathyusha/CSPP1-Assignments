f5c75031b2e5aede76343e142f0030e0b197083c
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-08-10 23:05:04 +0530

    " -> 0 of 6 passed. pylint: -1.18/10 "

--- M11/p2 2/assignment2.py
+++ M11/p2 2/assignment2.py
@@ -1,5 +1,8 @@
-#Exercise: Assignment-2
-#Implement the updateHand function. Make sure this function has no side effects: i.e., it must not mutate the hand passed in. Before pasting your function definition here, be sure you've passed the appropriate tests in test_ps4a.py.
+"""#Exercise: Assignment-2
+#Implement the updateHand function. Make sure
+this function has no side effects: i.e., it must not
+mutate the hand passed in. Before pasting your function
+definition here, be sure you've passed the appropriate tests in test_ps4a.py."""
 
 
 def updateHand(hand, word):
@@ -18,20 +21,24 @@ def updateHand(hand, word):
     hand: dictionary (string -> int)    
     returns: dictionary (string -> int)
     """
-    # TO DO ... <-- Remove this comment when you code this function
-    
+    a={}
+    for j in word:
+        if j in hand:
+            a[j] = hand[j]-1
+    return a
+
 
 def main():
-	n=input()
-	adict={}
-	for i in range(int(n)):
-		data=input()
-		l=data.split()
-		adict[l[0]]=int(l[1])
-	data1=input()
-	print(updateHand(adict,data1))
-		
+    n=input()
+    adict={}
+    for i in range(int(n)):
+        data=input()
+        l=data.split()
+        adict[l[0]]=int(l[1])
+    data1=input()
+    print(updateHand(adict,data1))
+        
 
 
 if __name__== "__main__":
-	main()
+    main()
