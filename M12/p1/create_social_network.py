8a978003d0e973ca3322bdfc6d38e33fd8e04570
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-08-11 12:16:48 +0530

    " -> 1 of 3 passed. pylint: 10.00/10 "

--- M12/p1/create_social_network.py
+++ M12/p1/create_social_network.py
@@ -42,8 +42,8 @@ def create_social_network(data):
             j = 0
             for j in range(len(l_ist)):
                 d_ict[i[j]] = i[j].split(",")
+        return d_ict
     return d_ict
-
 def main():
     '''
         handling testcase input and printing output
