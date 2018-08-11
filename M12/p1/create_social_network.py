0adede09b284c896e2d1e706a21c75e647e1832e
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-08-11 12:06:07 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- M12/p1/create_social_network.py
+++ M12/p1/create_social_network.py
@@ -41,7 +41,7 @@ def create_social_network(data):
         i = i.split("follows")
         j=0
         for j in range(len(l)):
-            if l[[j][0]] not in l
+            if l[[j][0]] not in d:
             l[[j][1]] = l[j][i].split(",")
             d[l[j][0]] = d[l[j][1]]
         elif d[j][1] not in d[j][0]:
