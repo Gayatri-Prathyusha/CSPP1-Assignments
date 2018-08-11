1b19ddb708bdb90a6172459c9c60965e9ee45947
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-08-11 10:42:29 +0530

    " -> 0 of 3 passed. pylint: 5.26/10 "

--- M12/p1/create_social_network.py
+++ M12/p1/create_social_network.py
@@ -38,7 +38,7 @@ def create_social_network(data):
     d = {}
     
     for i in l:
-        for j in len(l):
+        for j in int(len(l)):
             i = i.split("follows")
             d[i[j]] = i[j].split(", ")
     return d
