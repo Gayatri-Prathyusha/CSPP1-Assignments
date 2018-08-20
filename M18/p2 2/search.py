273e3ffd54182b4f74f42bf61cc98eed180a5cea
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-08-20 18:01:32 +0530

    " -> 0 of 1 passed. pylint: 4.74/10 "

--- M18/p2 2/search.py
+++ M18/p2 2/search.py
@@ -39,9 +39,9 @@ def search(search_index, query):
     '''
     query = query.lower().split(" ")
     list_new = []
-
-    for each_query  in search_index:
-        list_new = list_new.append(search_index[each_query])
+    if each_query in search_index:
+        for each_query in search_index:
+            list_new += [search_index[each_query]]
     return set(list_new)
     
 
